d607fe4464265bb6d071526936b99b15c4ac48c0
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2022-09-26 12:26:25 -0600

    adding first changes to JS adding the const's

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /*creating javascript file*/
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
