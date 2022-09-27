f5c36f5150d212bb5970afd95be3787979fd4deb
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2022-09-26 20:33:33 -0400

    added functions to js and added more questions

--- script.js
+++ script.js
@@ -24,6 +24,66 @@ function startGame() {
     setNextQuestion()
   }
 
+  /*adding next question functions*/
+
+  function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+  }
+
+  /*adding function to remove the display hide from css and showing the question content*/
+
+  function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
+
+  function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+  
+  function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+
 /*adding the questions below*/
 const questions = [
     {
@@ -85,7 +145,7 @@ const questions = [
         answers: [
           { text: 'git save changes', correct: false },
           /*check the answer below to make sure there isnt any errors*/
-          { text: 'git add .' correct: true },
+          { text: 'git add .', correct: true },
           { text: 'git -m save', correct: false },
           { text: 'git + changes', correct: false }
         ]
